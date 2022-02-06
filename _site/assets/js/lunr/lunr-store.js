2676488f9263977f7bf6e1b38367ee866a844f97
avcu <avcu@usc.edu>
2022-02-06 00:46:58 -0800

    adding a blind page for CSCI-571 HWs

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -6,7 +6,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Building GitHub Pages using Docker",
-        "excerpt":"This website is built using Jekyll with Minimal Mistakes theme and hosted on GitHub Pages. To run the website locally, one needs Ruby, RubyGems and Jekyll. Ruby is a Programming language, RubyGems is a package manager for Ruby just like pip for Python and finally Jekyll is a gem that...","categories": ["Programming"],
+        "excerpt":"This website is built using Jekyll with Minimal Mistakes theme and hosted on GitHub Pages. To run the website locally, one needs Ruby, RubyGems and Jekyll. Ruby is a programming language, RubyGems is a package manager for Ruby just like pip for Python and finally Jekyll is a gem that...","categories": ["Programming"],
         "tags": ["Github","Docker"],
         "url": "/programming/building-github-pages-using-docker/",
         "teaser": null
